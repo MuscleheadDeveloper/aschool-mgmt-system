143adb4afe819402372ffd5314046fb9e5ef57b7
muscleheadDeveloper <njokua04@gmail.com>
2023-01-13 16:25:34 +0100

    'login-UI-completed'

--- aschoolmgmtsystem/tailwind.config.cjs
+++ aschoolmgmtsystem/tailwind.config.cjs
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        '40-r': '40rem',
+        '30-r': '30rem'
+      },
+      height: {
+        '40-r': '40rem',
+        '30-r': '30rem'
+      },
+      boxShadow: {
+        '5xl': '20px 20px 50px rgba(0, 0, 0.5)'
+      },
+      fontFamily: {
+        'poppins': ['Poppins', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 };
